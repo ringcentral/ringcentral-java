b1f62641b607c4495905d726db630fda48561aaa
Tyler Long <tyler4long@gmail.com>
2017-06-08 14:10:33 +0800

    Write url builder tests

--- src/test/java/com/ringcentral/UrlBuilderTest.java
+++ src/test/java/com/ringcentral/UrlBuilderTest.java
@@ -4,9 +4,14 @@ import com.ringcentral.definitions.CallerInfo;
 import com.ringcentral.definitions.MessageInfo;
 import com.ringcentral.definitions.VersionInfo;
 import com.ringcentral.paths.Sms;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
 
@@ -36,9 +41,21 @@ public class UrlBuilderTest extends BaseTest {
     }
 
     @Test
-    public void TestSms() {
+    public void TestSms() throws IOException, RestException {
         String endpoint = restClient.restApi().account().extension().sms().endpoint();
         assertEquals("/restapi/v1.0/account/~/extension/~/sms", endpoint);
+
+        Sms.PostParameters postParameters = new Sms.PostParameters();
+        postParameters.from = new CallerInfo().phoneNumber(config.get("username"));
+        postParameters.to = new CallerInfo[]{new CallerInfo().phoneNumber(config.get("receiver"))};
+        postParameters.text = "hello world";
+
+        MessageInfo messageInfo = restClient.restApi().account().extension().sms().post(postParameters, MessageInfo.class);
+        assertNotNull(messageInfo);
+        assertEquals("SMS", messageInfo.type);
+
+        String result = restClient.restApi().account().extension().sms().post(postParameters).string();
+        assertTrue(result.contains("SMS"));
     }
 
     @Test
@@ -49,13 +66,13 @@ public class UrlBuilderTest extends BaseTest {
     }
 
     @Test
-    public void TestPost() throws IOException, RestException {
-        Sms.PostParameters postParameters = new Sms.PostParameters();
-        postParameters.from = new CallerInfo().phoneNumber(config.get("username"));
-        postParameters.to = new CallerInfo[]{new CallerInfo().phoneNumber(config.get("receiver"))};
-        postParameters.text = "hello world";
-        MessageInfo messageInfo = restClient.restApi().account().extension().sms().post(postParameters, MessageInfo.class);
-        assertNotNull(messageInfo);
-        assertEquals("SMS", messageInfo.type);
+    public void TestFax() throws IOException, RestException {
+        RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.MIXED)
+            .addPart(RequestBody.create(MediaType.parse("application/json"),
+                "{ \"to\": [{ \"phoneNumber\": " + config.get("receiver") + " }]}"))
+            .addFormDataPart("attachment", "test.txt", RequestBody.create(MediaType.parse("text/plain"), "Hello world"))
+            .addFormDataPart("attachment", "test.png", RequestBody.create(MediaType.parse("image/png"), Files.readAllBytes(Paths.get("./src/test/resources/test.png"))))
+            .build();
+        restClient.restApi().account().extension().fax().post(requestBody);
     }
 }

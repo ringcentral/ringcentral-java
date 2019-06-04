e962ceb60624977df4a2f134e6e1f967c706f108
Tyler Long <tyler4long@gmail.com>
2019-06-04 15:35:11 -0700

    Add ivr tests

---
+++ src/test/java/com/ringcentral/IVRContentTest.java
@@ -0,0 +1,108 @@
+package com.ringcentral;
+
+import com.ringcentral.definitions.*;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class IVRContentTest {
+    @Test
+    public void downloadIVRPromptContent() throws IOException, RestException {
+        RestClient rc = new RestClient(
+            System.getenv("RINGCENTRAL_CLIENT_ID"),
+            System.getenv("RINGCENTRAL_CLIENT_SECRET"),
+            System.getenv("RINGCENTRAL_SERVER_URL")
+        );
+
+        rc.authorize(
+            System.getenv("RINGCENTRAL_USERNAME"),
+            System.getenv("RINGCENTRAL_EXTENSION"),
+            System.getenv("RINGCENTRAL_PASSWORD")
+        );
+
+        IVRPrompts ivrPrompts = rc.restapi().account().ivrprompts().list();
+        PromptInfo promptInfo = rc.restapi().account().ivrprompts(ivrPrompts.records[0].id).get();
+        assertNotNull(promptInfo.id);
+        assertEquals(promptInfo.id, ivrPrompts.records[0].id);
+
+//        ResponseBody responseBody = rc.get("/restapi/v1.0/account/~/ivr-prompts/"+ promptInfo.id + "/content");
+//        byte[] bytes = responseBody.bytes();
+//        assertTrue(bytes.length > 0);
+        rc.revoke();
+    }
+
+    @Test
+    public void createIVRPromptContent() throws IOException, RestException {
+        RestClient rc = new RestClient(
+            System.getenv("RINGCENTRAL_CLIENT_ID"),
+            System.getenv("RINGCENTRAL_CLIENT_SECRET"),
+            System.getenv("RINGCENTRAL_SERVER_URL")
+        );
+
+        rc.authorize(
+            System.getenv("RINGCENTRAL_USERNAME"),
+            System.getenv("RINGCENTRAL_EXTENSION"),
+            System.getenv("RINGCENTRAL_PASSWORD")
+        );
+
+        rc.httpEventListeners.add(new HttpEventListener() {
+            @Override
+            public void afterHttpCall(Response response, Request request) {
+                String httpMessage = Utils.formatHttpMessage(response,request);
+            }
+        });
+
+        PromptInfo promptInfo = rc.restapi().account().ivrprompts().post(new CreateIvrPromptRequest()
+            .name("Uploaded via API")
+            .attachment(new Attachment()
+                .contentType("audio/mpeg")
+                .fileName("test.mp3")
+                .bytes(Files.readAllBytes(Paths.get("./src/test/resources/test.mp3")))
+            ));
+
+        rc.revoke();
+    }
+
+    @Test
+    public void updateIVRPromptContent() throws IOException, RestException {
+        RestClient rc = new RestClient(
+            System.getenv("RINGCENTRAL_CLIENT_ID"),
+            System.getenv("RINGCENTRAL_CLIENT_SECRET"),
+            System.getenv("RINGCENTRAL_SERVER_URL")
+        );
+
+        rc.authorize(
+            System.getenv("RINGCENTRAL_USERNAME"),
+            System.getenv("RINGCENTRAL_EXTENSION"),
+            System.getenv("RINGCENTRAL_PASSWORD")
+        );
+
+        rc.httpEventListeners.add(new HttpEventListener() {
+            @Override
+            public void afterHttpCall(Response response, Request request) {
+                String httpMessage = Utils.formatHttpMessage(response,request);
+            }
+        });
+
+        IVRPrompts ivrPrompts = rc.restapi().account().ivrprompts().list();
+        PromptInfo promptInfo = rc.restapi().account().ivrprompts(ivrPrompts.records[0].id).get();
+        assertNotNull(promptInfo.id);
+
+//        PromptInfo promptInfo = rc.restapi().account().ivrprompts().put(new CreateIvrPromptRequest()
+//            .name("Uploaded via API")
+//            .attachment(new Attachment()
+//                .contentType("audio/mpeg")
+//                .fileName("test.mp3")
+//                .bytes(Files.readAllBytes(Paths.get("./src/test/resources/test.mp3")))
+//            ));
+
+        rc.revoke();
+    }
+}
